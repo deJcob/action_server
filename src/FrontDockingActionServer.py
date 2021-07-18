5024c31e8f211e508eeed5701d587c3bad9cf82b
Jakub Kakiel <kubakakiel@gmail.com>
2021-07-18 20:39:58 +0200

    Base prototype of driving server for robot

--- src/FrontDockingActionServer.py
+++ src/FrontDockingActionServer.py
@@ -2,25 +2,43 @@
 import rospy
 import actionlib
 import action_server.msg
+import copy
+import math
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import JointState
+
 
 class FrontDockingAction(object):
     # create messages that are used to publish feedback/result
     _feedback = action_server.msg.FrontDockingFeedback()
     _result = action_server.msg.FrontDockingResult()
+    _odom = Odometry()
+    _start_pos = Odometry()
+    _joint_states = JointState()
+    _inited = False
 
     def __init__(self, name):
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name, action_server.msg.FrontDockingAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
+
+        try:
+            rospy.Subscriber("/diff_drive/odom", Odometry, self.callbackOdom)
+            rospy.Subscriber("/joint_states/", JointState, self.callbackJoints)
+            self._pub = rospy.Publisher('/diff_drive/cmd_vel', Twist, queue_size=10) 
+        except rospy.ROSInterruptException:
+            exit
         rospy.loginfo('%s: Server started!' % self._action_name)
       
     def execute_cb(self, goal):
         # helper variables
         r = rospy.Rate(100)
-        success = True
+        success = False
         
         # init feedback message
         self._feedback.distTraveled = 0.0
+        self._start_pos = copy.copy(self._odom)
 
         # publish info to the console for the user
         start_msg = ''
@@ -38,22 +56,55 @@ class FrontDockingAction(object):
             goal.velNormal, goal.distToDocking, goal.velDocking)
         
         # start executing the action
-        for i in range(1, goal.order):
+        while not success: 
+            cmd_vel = Twist()
+            cmd_vel.linear.x = goal.velNormal
+
+            x = abs(self._start_pos.pose.pose.position.x - self._odom.pose.pose.position.x)
+            y = abs(self._start_pos.pose.pose.position.y - self._odom.pose.pose.position.y)
+            self._feedback.distTraveled = math.sqrt(x*x+y*y)
+
             # check that preempt has not been requested by the client
             if self._as.is_preempt_requested():
                 rospy.loginfo('%s: Preempted' % self._action_name)
                 self._as.set_preempted()
+                cmd_vel.linear.x = 0.0
+                self._pub.publish(cmd_vel) 
                 success = False
                 break
-            self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
+
+            # TODO in future change it to readings from laser ruler (!)
+            self._feedback.distToDock = goal.distToStop - self._feedback.distTraveled 
+            
+            # Moving in docking zone 
+            if goal.distToStop - self._feedback.distTraveled < goal.distToDocking:
+                cmd_vel.linear.x = goal.velDocking
+
+            # brakig zone
+            if goal.distToStop - self._feedback.distTraveled < 0.05:
+                cmd_vel.linear.x = 0.0
+                if self._joint_states.velocity[0] == 0 and self._joint_states.velocity[1] == 0:
+                    success = True
+
+            # publish seted velocity
+            self._pub.publish(cmd_vel) 
             # publish the feedback
             self._as.publish_feedback(self._feedback)
+        
             r.sleep()
-          
+
         if success:
-            self._result.sequence = self._feedback.sequence
-            rospy.loginfo('%s: Goal reached' % self._action_name)
+            self._result.distTraveled = self._feedback.distTraveled
+            self._result.distToDock = self._feedback.distToDock
+
+            rospy.loginfo('%s: Goal reached. %f [m] to docking, %f [m] traveled', self._action_name, self._result.distToDock, self._result.distTraveled)
             self._as.set_succeeded(self._result)
+
+    def callbackOdom(self, data):
+        self._odom = data 
+
+    def callbackJoints(self, data):
+        self._joint_states = data
         
 if __name__ == '__main__':
     rospy.init_node('FrontDocking')
